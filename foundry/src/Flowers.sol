9430e3b7ecd7c3855c77cf475bed101658fc394c
Olivier Toscano <olivier@toscano.lu>
2023-01-18 13:30:04 +0100

    add Flower smart contract

---
+++ foundry/src/Flowers.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
+
+/// @custom:security-contact olivier@toscano.lu
+contract Flowers is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("flowers", "FLO") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://static.webcms.lu/olivier/nftFlowers/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function buyToken() public payable {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(msg.value == tokenId * 0.1 ether, "Not enough funds sent");
+
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override(ERC721)
+        returns (string memory)
+    {
+        return string(abi.encodePacked(_baseURI(), "flower", Strings.toString(tokenId+1), ".json"));
+    }
+}
